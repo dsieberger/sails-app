1ef91279de2b123a705189a9c164ea17612af22a
David Sieberger <d.sieberger@campus.fct.unl.pt>
2014-09-21 18:11:26 +0100

    User resource created.

---
+++ config/paypal.js
@@ -0,0 +1,10 @@
+
+module.exports.paypal = {
+
+	mode: 'sandbox',
+
+	client_id: 'AQo2uxB8WOGh8pPjz3LojZUgdalWUYKReh9Oxi6c5QZy6pPafrjsJdWaNYVF',
+
+	client_secret: 'EG9GaRAqM2p8EgfGLR4PpyW9un_FkZXq0mbWrX7Ssj1ieY5QyElodTMVDd8Q'
+	
+}
